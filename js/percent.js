08f445bb007c6753ad889c006e6df317391c0f46
rafgpereira <rafaelgomespereira123@gmail.com>
2023-08-13 16:34:59 -0300

    functions upgrade

--- js/percent.js
+++ js/percent.js
@@ -1,136 +1,151 @@
 //Geração de dados úteis para o funcionamento:
-geraDados()
-function geraDados() {
-    let novaData = new Date()
-    let dia = novaData.getDate()
-    let mes = novaData.getMonth()
-    let ano = novaData.getFullYear()
-    let hora = novaData.getHours() + 1
-    let minuto = novaData.getMinutes() + 1
-    const numeroSecreto = parseInt((ano+(mes*dia**3))%100)
-    const dadosGerados = {
-        dia: dia,
-        mes: mes,
-        ano: ano,
-        hora: hora,
-        minuto: minuto,
-        numeroSecreto: numeroSecreto
-    }
-    document.body.style.setProperty("--progress", dadosGerados.numeroSecreto);
-    return dadosGerados
-}
+function geraData() {
+  const novaData = new Date();
+  const dia = novaData.getDate();
+  const mes = novaData.getMonth();
+  const ano = novaData.getFullYear();
+  const hora = (23 - novaData.getHours()).toString();
+  const minuto = (59 - novaData.getMinutes()).toString();
+  const segundo = (59 - novaData.getSeconds()).toString();
 
+  return { dia, mes, ano, hora, minuto, segundo };
+}
+function editaHora() {
+  const { hora, minuto, segundo } = geraData();
+  return `${hora.padStart(2, "0")}:${minuto.padStart(2,"0")}:${segundo.padStart(2, "0")}`;
+}
+function geraNumeroSecreto() {
+  const { dia, mes, ano } = geraData();
+  const numeroSecreto = parseInt((ano + mes * dia ** 3) % 100);
+  document.body.style.setProperty("--progress", numeroSecreto);
+  return numeroSecreto;
+}
 //Declarações:
-const dados = geraDados()
-const informacaoDica = document.querySelector(".dica")
-const informacaoTentativa = document.querySelector(".tentativas-restantes")
-const botaoChute = document.querySelector("#guess")
-const campoEntrada = document.querySelector("#numInput")
-const popupInicial = document.querySelector("#bem-vindo")
-const botaoPopupInicial = document.querySelector("#botao-popup-iniciar")
-
-//Controle do Local Storage:
-if (!localStorage.getItem(`${dados.dia}${dados.mes}${dados.ano}`)) {
-    localStorage.setItem(`${dados.dia}${dados.mes}${dados.ano}`, "naojogou")
-    popupInicial.showModal()
-    botaoPopupInicial.addEventListener("click", ()=>{
-        popupInicial.close()
-    })
-} else if (localStorage.getItem(`${dados.dia}${dados.mes}${dados.ano}`) == "jogou") {
-    removeEntrada()
-    tempoNovoJogo()
-}
-if (!localStorage.getItem(`${dados.ano}${dados.mes}${dados.dia}`)) {
-    localStorage.setItem(`${dados.ano}${dados.mes}${dados.dia}`, 3)
+function declaracoes() {
+  const informacaoDica = document.querySelector(".dica");
+  const informacaoTentativa = document.querySelector(".tentativas-restantes");
+  const botaoChute = document.querySelector("#guess");
+  const campoEntrada = document.querySelector("#numInput");
+  const popup = document.querySelector("#popup");
+  const popupTitulo = document.querySelector("#h2-popup");
+  const popupTexto = document.querySelector("#p-popup");
+  const popupBotao = document.querySelector("#botao-popup");
+  return { informacaoDica, informacaoTentativa, botaoChute, campoEntrada, popup, popupTitulo, popupTexto, popupBotao}
 }
 
-//Escutas de eventos do botão de chute:
-botaoChute.addEventListener("click", function () {
-    if (localStorage.getItem(`${dados.dia}${dados.mes}${dados.ano}`) == "naojogou") {
-        verificaTentativas()
-    }
-})
-campoEntrada.addEventListener("keydown", function verificaTecla(event){
-    if (event.key == "Enter") {
-        if (localStorage.getItem(`${dados.dia}${dados.mes}${dados.ano}`) == "naojogou") {
-            verificaTentativas()
-        }
-    }
-});
-
-//Função que analisa as tentativas restantes:
-function verificaTentativas() {
-    let tentativasRestantes = parseInt(localStorage.getItem(`${dados.ano}${dados.mes}${dados.dia}`))
-    if (tentativasRestantes > 0) {
-        verificaChute(tentativasRestantes)
-    }
+function tentativasDiarias() {
+  const { dia, mes, ano } = geraData();
+  const {popup, popupBotao} = declaracoes()
+  if (!localStorage.getItem(`${ano}${mes}${dia}`)) {
+    localStorage.setItem(`${ano}${mes}${dia}`, 3);
+    popup.showModal();
+    popupBotao.addEventListener("click", () => {
+      popup.close();
+    });
+  } else if (localStorage.getItem(`${ano}${mes}${dia}`) == 0) {
+    removeEntrada();
+    tempoNovoJogo();
+  }
 }
 
 //Função que verifica o chute e responde:
 function verificaChute(tentativasRestantes) {
-    const numeroChute = campoEntrada.value
-    if (numeroChute == dados.numeroSecreto) {
-        localStorage.setItem(`${dados.dia}${dados.mes}${dados.ano}`, "jogou")
-        removeEntrada()
-        avisoVitoria(tentativasRestantes)
-    } else if (numeroChute != dados.numeroSecreto) {
-        if (numeroChute < dados.numeroSecreto) {
-            informacaoDica.innerHTML = `Muito baixo!`
-            informacaoDica.style.color = `#ff1b1b`
-            informacaoTentativa.innerHTML = `Tentativas restantes: ${tentativasRestantes - 1}`
-        } else {
-            informacaoDica.innerHTML = `Muito alto!`
-            informacaoDica.style.color = `#00e1ff`
-            informacaoTentativa.innerHTML = `Tentativas restantes: ${tentativasRestantes - 1}`
-        }
-        tentativasRestantes--
-        localStorage.setItem(`${dados.ano}${dados.mes}${dados.dia}`, tentativasRestantes)
-        if (tentativasRestantes == 0) {
-            removeEntrada()
-            localStorage.setItem(`${dados.dia}${dados.mes}${dados.ano}`, "jogou")
-            avisoDerrota()
-        }
+  const { dia, mes, ano } = geraData();
+  const {informacaoDica, informacaoTentativa, campoEntrada} = declaracoes()
+  const numeroChute = campoEntrada.value;
+  const numeroSecreto = geraNumeroSecreto();
+  if (numeroChute == numeroSecreto) {
+    localStorage.setItem(`${ano}${mes}${dia}`, 0);
+    removeEntrada();
+    avisoVitoria(tentativasRestantes);
+  } else if (numeroChute != numeroSecreto) {
+    if (numeroChute < numeroSecreto) {
+      informacaoDica.innerHTML = `Muito baixo!`;
+      informacaoDica.style.color = `#ff1b1b`;
+      informacaoTentativa.innerHTML = `Tentativas restantes: ${
+        tentativasRestantes - 1
+        
+      }`;
+    } else {
+      informacaoDica.innerHTML = `Muito alto!`;
+      informacaoDica.style.color = `#00e1ff`;
+      informacaoTentativa.innerHTML = `Tentativas restantes: ${
+        tentativasRestantes - 1
+      }`;
     }
-    campoEntrada.value = ""
+    tentativasRestantes--;
+    localStorage.setItem(`${ano}${mes}${dia}`, tentativasRestantes);
+    if (tentativasRestantes == 0) {
+        tentativasDiarias()
+      avisoDerrota();
+    }
+  }
+  campoEntrada.value = "";
+  main()
 }
 
 //Função que remove os Inputs apos o jogo:
 function removeEntrada() {
-    botaoChute.remove()
-    campoEntrada.remove()
-    informacaoTentativa.remove()
+    const {botaoChute, informacaoTentativa, campoEntrada} = declaracoes()
+  botaoChute.remove();
+  campoEntrada.remove();
+  informacaoTentativa.remove();
 }
-
-//Declarações para o popup:
-const popup = document.querySelector("#popup")
-const popupTitulo = document.querySelector("#h2-popup")
-const popupTexto = document.querySelector("#p-popup")
-const popupBotao = document.querySelector("#botao-popup");
 
 //Funções que apresentam os popup's:
 function avisoVitoria(tentativasRestantes) {
-    popupBotao.addEventListener("click", () => {
-        popup.close()
-    });
-    setTimeout(() => {
-        popup.showModal()
-        popupTitulo.innerHTML = "Parabéns!"
-        popupTexto.innerHTML = `O número secreto é ${dados.numeroSecreto}! </br> Tentativas utilizadas: ${4 - tentativasRestantes}`
-    }, 0);
-    tempoNovoJogo()
+    const {popup, popupBotao, popupTexto, popupTitulo} = declaracoes()
+  popupBotao.addEventListener("click", () => {
+    popup.close();
+  });
+  setTimeout(() => {
+    popup.showModal();
+    popupTitulo.innerHTML = "Parabéns!";
+    popupTexto.innerHTML = `O número secreto é ${geraNumeroSecreto()}! </br> Tentativas utilizadas: ${4 - tentativasRestantes}`;
+  }, 0);
+  tempoNovoJogo();
 }
 function avisoDerrota() {
-    popupBotao.addEventListener("click", () => {
-        popup.close()
-    });
-    setTimeout(() => {
-        popup.showModal()
-        popupTitulo.innerHTML = "Ops!"
-        popupTexto.innerHTML = `As tentativas acabaram! </br> O número secreto era ${dados.numeroSecreto}. </br> Tente novamente amanhã.`
-    }, 0);
-    tempoNovoJogo()
+    const {popup, popupBotao, popupTexto, popupTitulo} = declaracoes()
+  popupBotao.addEventListener("click", () => {
+    popup.close();
+  });
+  setTimeout(() => {
+    popup.showModal();
+    popupTitulo.innerHTML = "Ops!";
+    popupTexto.innerHTML = `As tentativas acabaram! </br> O número secreto era ${geraNumeroSecreto()}. </br> Tente novamente amanhã.`;
+  }, 0);
+  tempoNovoJogo();
 }
 function tempoNovoJogo() {
-    informacaoDica.innerHTML = `Novo jogo em ${24 - dados.hora} hora(s) e ${60 - dados.minuto} minuto(s).`
-    informacaoDica.style.color = `#f1c40f`
+    const {informacaoDica} = declaracoes() 
+  setInterval(() => {
+    const tempoRestante = editaHora();
+    informacaoDica.innerHTML = `Novo jogo em </br> ${tempoRestante}`;
+  }, 0);
+
+  informacaoDica.style.color = `#f1c40f`;
 }
+
+function main() {
+  geraNumeroSecreto();
+  tentativasDiarias();
+  const {botaoChute, campoEntrada} = declaracoes()
+  const { dia, mes, ano } = geraData();
+  let tentativasRestantes = parseInt(
+    localStorage.getItem(`${ano}${mes}${dia}`)
+  );
+  botaoChute.addEventListener("click", function () {
+    if (localStorage.getItem(`${ano}${mes}${dia}`) > 0) {
+      verificaChute(tentativasRestantes);
+    }
+  });
+  campoEntrada.addEventListener("keydown", function verificaTecla(event) {
+    if (event.key == "Enter") {
+      if (localStorage.getItem(`${ano}${mes}${dia}`) > 0) {
+        verificaChute(tentativasRestantes);
+      }
+    }
+  });
+}
+main();
